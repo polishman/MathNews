abdd7c8a6f7a7d8303634eda98f31b83ec50c72a
polishman <stepanbanach@gmail.com>
2018-09-25 15:36:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -144,6 +144,12 @@ var json = {
 
 window.survey = new Survey.Model(json);
 
-
+survey
+    .onComplete
+    .add(function (result) {
+        document
+            .querySelector('#surveyResult')
+            .innerHTML = "result: " + JSON.stringify(result.data);
+    });
 
 $("#surveyElement").Survey({model: survey});
