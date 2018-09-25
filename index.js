cf65141c1e5909c538298563e9057f0d1c9ef5c6
polishman <stepanbanach@gmail.com>
2018-09-25 17:51:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ var elk=["\u0442\u0430\u0440\u0441-\u0442\u0430\u0440\u0441"];
 var shl=["\u0430\u043B\u0430\u0439\u0434\u0430"]; 
 var odg=["\u044F\u043F\u044B\u0440\u043C\u0430\u0443"]; 
 
+var at=["\u0435\u04A3\u0431\u0435\u043A"];
+var il=["\u0435\u04A3\u0431\u0435\u043A\u0442\u0456\u04A3"];
+var br=["\u0435\u04A3\u0431\u0435\u043A\u043A\u0435"];
+var tb=["\u0435\u04A3\u0431\u0435\u043A\u0442\u0456"];
+var zh=["\u0435\u04A3\u0431\u0435\u043A\u0442\u0435"];
+var sh=["\u0435\u04A3\u0431\u0435\u043A\u0442\u0435\u043D"];
+var km=["\u0435\u04A3\u0431\u0435\u043A\u043F\u0435\u043D"]; 
+
 function rnd(a) {
 return a[Math.floor(Math.random()*a.length)];
 }
@@ -50,7 +58,7 @@ var json = {
                     name: "2",
                     title: "\u0421\u044B\u043D \u0435\u0441\u0456\u043C \u0441\u04E9\u0437 \u0442\u0430\u0431\u044B\u043D\u0430 \u043D\u0435 \u0436\u0430\u0442\u0430\u0434\u044B?", 
                     choices: [
-                        "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+                        rnd(odg), rnd(noun), rnd(adj)/**/, rnd(verb)
                     ]
                 }, 
                 {
@@ -58,7 +66,7 @@ var json = {
                     name: "3",
                     title: "\u0421\u0430\u043D \u0435\u0441\u0456\u043C \u0441\u04E9\u0437 \u0442\u0430\u0431\u044B\u043D\u0430 \u043D\u0435 \u0436\u0430\u0442\u0430\u0434\u044B?",
                     choices: [
-                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
+                      rnd(adj), rnd(shl), rnd(numerl)/**/, rnd(ust)  
                     ]
  
                 }, {
@@ -66,28 +74,28 @@ var json = {
                     type: "radiogroup",
                     name: "4",
                     title: "\u0415\u0441\u0456\u043C\u0434\u0456\u043A \u0441\u04E9\u0437 \u0442\u0430\u0431\u044B\u043D\u0430 \u043D\u0435 \u0436\u0430\u0442\u0430\u0434\u044B?",
-                    choices: [rnd(noun), rnd(adj), rnd(verb), rnd(ust)
+                    choices: [rnd(noun), rnd(adj), rnd(verb), rnd(pron)/**/
                     ]
                 }, {
                           
                     type: "radiogroup",
                     name: "5",
                     title: "\u0415\u0442\u0456\u0441\u0442\u0456\u043A \u0441\u04E9\u0437 \u0442\u0430\u0431\u044B\u043D\u0430 \u043D\u0435 \u0436\u0430\u0442\u0430\u0434\u044B?",
-                    choices: [rnd(noun), rnd(adj), rnd(verb), rnd(ust)
+                    choices: [rnd(noun), rnd(adj), rnd(verb)/**/, rnd(ust)
                     ]
                 }, {
                           
                     type: "radiogroup",
                     name: "6",
                     title: "\u04AE\u0441\u0442\u0435\u0443 \u0441\u04E9\u0437 \u0442\u0430\u0431\u044B\u043D\u0430 \u043D\u0435 \u0436\u0430\u0442\u0430\u0434\u044B?",
-                    choices: [rnd(noun), rnd(adj), rnd(verb), rnd(ust)
+                    choices: [rnd(odg), rnd(adj), rnd(verb), rnd(ust)/**/
                     ]
                 },{
                           
                     type: "radiogroup",
                     name: "7",
                     title: "\u0415\u043B\u0456\u043A\u0442\u0435\u0443 \u0441\u04E9\u0437 \u0441\u04E9\u0437 \u0442\u0430\u0431\u044B\u043D\u0430 \u043D\u0435 \u0436\u0430\u0442\u0430\u0434\u044B?",
-                    choices: [rnd(noun), rnd(adj), rnd(verb), rnd(ust)
+                    choices: [rnd(elk)/**/, rnd(adj), rnd(verb), rnd(ust)
                     ]
                 }, 
                 {
@@ -95,7 +103,7 @@ var json = {
                     type: "radiogroup",
                     name: "8",
                     title: "\u0428\u044B\u043B\u0430\u0443 \u0441\u04E9\u0437 \u0442\u0430\u0431\u044B\u043D\u0430 \u043D\u0435 \u0436\u0430\u0442\u0430\u0434\u044B?",
-                    choices: [rnd(noun), rnd(adj), rnd(verb), rnd(ust)
+                    choices: [rnd(noun), rnd(shl)/**/, rnd(verb), rnd(ust)
                     ]
                 }
                 ,
@@ -104,9 +112,52 @@ var json = {
                     type: "radiogroup",
                     name: "9",
                     title: "\u041E\u0434\u0430\u0493\u0430\u0439 \u0441\u04E9\u0437 \u0442\u0430\u0431\u044B\u043D\u0430 \u043D\u0435 \u0436\u0430\u0442\u0430\u0434\u044B?",
-                    choices: [rnd(noun), rnd(adj), rnd(verb), rnd(ust)
+                    choices: [rnd(odg)/**/, rnd(adj), rnd(verb), rnd(ust)
                     ]
+                }, 
+                {
+                    type: "radiogroup", 
+                    name: "10",
+                    title: "\u0410\u0442\u0430\u0443 \u0441\u0435\u043F\u0442\u0456\u043A\u0442\u0435\u0433\u0456 \u0437\u0430\u0442 \u0435\u0441\u0456\u043C\u0434\u0456 \u0442\u0430\u04A3\u0434\u0430\u043D\u044B\u0437:", 
+                    choices: [rnd(at)/***/, rnd(br), rn(il), rn(sh)]
+                },
+                 {
+                    type: "radiogroup", 
+                    name: "11",
+                    title: "\u0406\u043B\u0456\u043A \u0441\u0435\u043F\u0442\u0456\u043A\u0442\u0435\u0433\u0456 \u0437\u0430\u0442 \u0435\u0441\u0456\u043C\u0434\u0456 \u0442\u0430\u04A3\u0434\u0430\u043D\u044B\u0437:", 
+                    choices: [rnd(at), rnd(br), rn(il)/***/, rn(sh)]
+                },
+                 {
+                    type: "radiogroup", 
+                    name: "12",
+                    title: "\u0411\u0430\u0440\u044B\u0441 \u0441\u0435\u043F\u0442\u0456\u043A\u0442\u0435\u0433\u0456 \u0437\u0430\u0442 \u0435\u0441\u0456\u043C\u0434\u0456 \u0442\u0430\u04A3\u0434\u0430\u043D\u044B\u0437:", 
+                    choices: [rnd(at), rnd(br)/***/, rn(il), rn(sh)]
+                },
+                 {
+                    type: "radiogroup", 
+                    name: "13",
+                    title: "\u0422\u0430\u0431\u044B\u0441 \u0441\u0435\u043F\u0442\u0456\u043A\u0442\u0435\u0433\u0456 \u0437\u0430\u0442 \u0435\u0441\u0456\u043C\u0434\u0456 \u0442\u0430\u04A3\u0434\u0430\u043D\u044B\u0437:", 
+                    choices: [rnd(tb)/***/, rnd(br), rn(il), rn(sh)]
+                },
+                 {
+                    type: "radiogroup", 
+                    name: "14",
+                    title: "\u0416\u0430\u0442\u044B\u0441 \u0441\u0435\u043F\u0442\u0456\u043A\u0442\u0435\u0433\u0456 \u0437\u0430\u0442 \u0435\u0441\u0456\u043C\u0434\u0456 \u0442\u0430\u04A3\u0434\u0430\u043D\u044B\u0437:", 
+                    choices: [rnd(at), rnd(br), rn(il), rn(zh)/**/]
                 }
+                , {
+                    type: "radiogroup", 
+                    name: "15",
+                    title: "\u0428\u044B\u0493\u044B\u0441 \u0441\u0435\u043F\u0442\u0456\u043A\u0442\u0435\u0433\u0456 \u0437\u0430\u0442 \u0435\u0441\u0456\u043C\u0434\u0456 \u0442\u0430\u04A3\u0434\u0430\u043D\u044B\u0437:", 
+                    choices: [rnd(at), rnd(br), rn(il), rn(sh)/**/]
+                },
+                 {
+                    type: "radiogroup", 
+                    name: "16",
+                    title: "\u041A\u04E9\u043C\u0435\u043A\u0442\u0435\u0441 \u0441\u0435\u043F\u0442\u0456\u043A\u0442\u0435\u0433\u0456 \u0437\u0430\u0442 \u0435\u0441\u0456\u043C\u0434\u0456 \u0442\u0430\u04A3\u0434\u0430\u043D\u044B\u0437:", 
+                    choices: [rnd(km)/***/, rnd(br), rn(il), rn(sh)]
+                }
+                
 
             ]
         }
